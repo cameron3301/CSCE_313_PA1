6b8050e45e0a854938ff7117e655ff81ca21dbae
Cameron Coffey <cameron3301@yahoo.com>
2019-09-17 12:17:05 -0500

    Fixed deconstructor code

--- BuddyAllocator.cpp
+++ BuddyAllocator.cpp
@@ -48,10 +48,6 @@ BuddyAllocator::BuddyAllocator (int _basic_block_size, int _total_memory_length)
 
 
 BuddyAllocator::~BuddyAllocator (){
-  for (int i = FreeList.size()-1; i >= 0 ; i++) {
-    delete FreeList[i];
-  }
-
   delete [] memoryStart;
 }
 
