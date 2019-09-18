800536ec3bf9f4ef5ab61f8f2afba605cfb4747e
Cameron Coffey <cameron3301@yahoo.com>
2019-09-18 16:23:26 -0500

    partial test for optarg functionality 7

--- Main.cpp
+++ Main.cpp
@@ -46,7 +46,7 @@ bool powerOfTwo(int val) {
 
   // increment `i` to the next power of 2
   while (i < val) {
-    i *= 2;
+    i = i * 2;
   }
 
   return (i == val);
@@ -55,11 +55,16 @@ bool powerOfTwo(int val) {
 
 // ...
 void validateInputArguments(int block_size, int mem_len) {
+
+  cout << "11111" << endl;
+
   // Test 1: is `block_size` a power of 2
   if (!powerOfTwo(block_size)) {
     // throw error
     throw invalid_argument("Input received for Basic Block Size is not a power of 2");
   }
+
+  cout << "22222" << endl;
 
   // Test 2: is `mem_len` a power of 2
   if (!powerOfTwo(mem_len)) {
@@ -67,17 +72,23 @@ void validateInputArguments(int block_size, int mem_len) {
     throw invalid_argument("Input received for Memory Length is not a power of 2");
   }
 
+  cout << "33333" << endl;
+
   // Test 3: is `mem_len` greater than `block_size`
   if (mem_len <= block_size) {
     // throw error
     throw invalid_argument("Value of Basic Block Size may not be greater than that of Memory Length");
   }
 
+  cout << "44444" << endl;
+
   // Test 4: is `mem_len` greater than 128 * 1024 * 1024
   if (mem_len < (128 * 1024 * 1024)) {
     // throw error
     throw invalid_argument("Value of Memory Length must be greater than or equal to 128 * 1024 * 1024 bytes");
   }
+
+  cout << "55555" << endl;
 }
 
 
@@ -117,6 +128,8 @@ int main(int argc, char ** argv) {
   } else {
     memory_length = 128 * 1024 * 1024;
   }
+
+  cout << "AAAAAA" << endl;
 
   validateInputArguments(basic_block_size, memory_length);
 
